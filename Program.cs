e5a53a1c2b671639998ee4b3c41c6d953c58da59
vic <vjohnson@ImageTrend.com>
2020-02-10 16:01:26 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace install_filesearch
+{
+    class Program
+    {
+        private const string FILE_NAME = "FileSearch.ps1";
+        private static bool CyanOrPink = true; // true = cyan - false = pink
+        static void Main(string[] args)
+        {
+            FlipColorAndWrite($"{DateTime.Now} - Installing FileSearch on PATH for user {Environment.UserName}...");
+
+            var localAppDataDirectory = Environment.GetEnvironmentVariable("LocalAppData");
+
+            var programAppDataDirectory = localAppDataDirectory + @"\Programs\";
+
+            var fileSearchInstallDirectory = programAppDataDirectory + "FileSearch";
+
+            var regexAddPath = Regex.Escape(fileSearchInstallDirectory);
+
+            var environmentVariables = Environment
+                .GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User)
+                .Split(';')
+                .Where(x =>
+                    !string.IsNullOrEmpty(x) &&
+                    !Regex.IsMatch(x, $"^{regexAddPath}?")
+                )
+                .Append(fileSearchInstallDirectory);
+
+            var newEnvironmentVariable = string.Join(";", environmentVariables);
+
+            FlipColorAndWrite($"{DateTime.Now} - Creating directory in {fileSearchInstallDirectory}...");
+
+            var fileSearchFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, FILE_NAME);
+
+            var fileSearchDir = Directory.CreateDirectory(fileSearchInstallDirectory);
+
+            var fileSearchDestination = new FileInfo($"{fileSearchDir.FullName}\\{FILE_NAME}"); 
+
+            if (fileSearchDestination.Exists)
+            {
+                FlipColorAndWrite($"{DateTime.Now} - Old version found... Overriding...");
+                try
+                {
+                    File.Delete(fileSearchDestination.FullName);
+                }
+                catch (IOException ex)
+                {
+                    FlipColorAndWrite($"{DateTime.Now} - {ex.Message}");
+                    return;
+                }
+            }
+
+            FlipColorAndWrite($"{DateTime.Now} - Copying file...");
+
+            File.Copy(fileSearchFile, fileSearchDestination.FullName);
+
+            FlipColorAndWrite($"{DateTime.Now} - File copied...");
+
+            FlipColorAndWrite($"{DateTime.Now} - Setting PATH variable to include FileSearch...");
+
+            Environment.SetEnvironmentVariable("Path", newEnvironmentVariable, EnvironmentVariableTarget.User);
+
+            FlipColorAndWrite($"{DateTime.Now} - PATH variable set...");
+
+            FlipColorAndWrite($"{DateTime.Now} - Complete...");
+
+            FlipColorAndWrite($"{DateTime.Now} - Open PowerShell to start using...");
+
+            Console.ReadLine();
+        }
+
+        static void FlipColorAndWrite(string message)
+        {
+            CyanOrPink = !CyanOrPink;
+            Console.ForegroundColor = CyanOrPink ? ConsoleColor.Cyan : ConsoleColor.Magenta;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ bin/Debug/FileSearch.ps1
@@ -0,0 +1,28 @@
+﻿if ($args.Length -eq 0) {return}
+
+$fileSearch = $true
+if ($args.IndexOf('-dr') -gt -1) {
+    $fileSearch = $false
+} 
+
+foreach ($arg in $args) {
+    if ($arg -notmatch '-dr') 
+    {
+        $param += "*$arg*"
+    }
+}
+
+$type = @({directory},{file})[$fileSearch]
+
+Write-Host "searching for $type - ""$($param.Replace('*', " ").Replace("  ", " ").Trim())"""
+
+$fileName = Get-ChildItem -Recurse -File:($fileSearch) -Directory:(!$fileSearch) -ErrorAction SilentlyContinue -Filter $param | Select-Object -ExpandProperty FullName -First 1
+if ($fileName) 
+{
+    Write-Host $fileName
+    start-process -filepath $fileName
+}
+else 
+{
+    Write-Host "the $type was not found"
+}
